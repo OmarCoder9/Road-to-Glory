2c0a9910e39c289bfbea655f9984c9c44f2bf5b7
Omar Ahmed <omar.ahmed9806@gmail.com>
2026-07-14 18:05:19 +0300

    Implement authentication routes and controller for user registration and login

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ const DBconnect = require("./config/DBConnect")
 const httpStatusText = require("./utils/httpStatusText")
 const organizerRoutes = require("./routes/organizer.routes")
 const attendeeRoutes = require("./routes/attendee.routes");
-<<<<<<< HEAD
 const authRouter = require("./routes/auth.routes");
-=======
 const adminRoutes = require("./routes/admin.routes")
->>>>>>> a15fc42304ba2f2903295b437e776f7b0dda9da3
+
 
 DBconnect()
 
@@ -20,11 +18,7 @@ app.use(express.json())
 app.use("/api/auth", authRouter);
 app.use("/api/organizer", organizerRoutes)
 app.use("/api/attendee", attendeeRoutes);
-<<<<<<< HEAD
-
-=======
 app.use("/api/admin", adminRoutes)
->>>>>>> a15fc42304ba2f2903295b437e776f7b0dda9da3
 
 app.use((req, res, next) => {
   return res
